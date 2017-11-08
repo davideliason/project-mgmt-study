ef24cc79dfdc81fd8d10e5ea76ef4111bf86951c
davideliason <davethemaker@gmail.com>
2017-11-08 07:57:33 -0800

    successfully create dumb MessageBar component

---
+++ src/redux/components/MessageBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class MessageBar extends React.Component {
+	render(){
+		return (
+				<div>
+					<p>Hi I am a MessageBar Component</p>
+				</div>
+			);
+	}
+}
