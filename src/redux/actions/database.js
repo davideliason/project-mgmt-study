6e00d6854a0a51ef98c95e9a4956e304d164b3b2
davideliason <davethemaker@gmail.com>
2017-12-03 17:50:05 -0800

    there were some changes from previous that weren't committed or pushed so I'm doing that now

--- src/redux/actions/database.js
+++ src/redux/actions/database.js
@@ -1,15 +1,14 @@
 import firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyB25Uky1Y40WuW9gDvGz_ULexp2XwhMV0s",
-    authDomain: "project-mgmt-study.firebaseapp.com",
-    databaseURL: "https://project-mgmt-study.firebaseio.com",
-    projectId: "project-mgmt-study",
-    storageBucket: "",
-    messagingSenderId: "179189599236"
+    apiKey: "AIzaSyCZBv3R55MKUJf4nntjd6C2CXAkSJqSqqQ",
+    authDomain: "project-mgmt-study-5698a.firebaseapp.com",
+    databaseURL: "https://project-mgmt-study-5698a.firebaseio.com",
+    projectId: "project-mgmt-study-5698a",
+    storageBucket: "project-mgmt-study-5698a.appspot.com",
+    messagingSenderId: "369664170585"
   };
-
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 const database = firebase.database();
 
 export default database;
