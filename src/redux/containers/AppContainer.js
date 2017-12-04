1ddbbee01084b2a508d6eef2ed29ecfa01812f6a
davideliason <davethemaker@gmail.com>
2017-12-04 08:30:01 -0800

    ***Going to pause here until figuring out why I can't seem to access the state array of objects without error

--- src/redux/containers/AppContainer.js
+++ src/redux/containers/AppContainer.js
@@ -4,17 +4,20 @@ import {setKaFilter} from '../actions/get_steps.js';
 import {setPgFilter} from '../actions/get_steps.js';
 import App from '../../client/App.js';
 
-function kaFilterThis(state,filter){
-    for(const prop in state){
-      return state[prop] === filter
+function kaFilterThis(state){
+    var returnedArr = [];
+    
+    for(let obj of state){
+      returnedArr.push(obj)
     }
+    return returnedArr;
 }
 
 function mapStateToProps(state) {
   return {
     // steps : filterSteps(state.steps.stepgroups, state.pgFilter,state.kaFilter)
-    steps : state.steps.stepgroups,
-    kaFiltered : kaFilterThis(state.steps.stepgroups,state.kaFilter)
+    steps : state.steps.stepgroups
+    // kaFiltered : kaFilterThis(state.steps.stepgroups)
   }
 }
 
